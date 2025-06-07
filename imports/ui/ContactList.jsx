8acc324a2b34688e3733486b8fba7458a24c15d5
Yannick Tirand <ytirand@gmail.com>
2025-06-07 11:08:23 +0200

    Removes autopublish and uses publications instead

--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { ContactsCollection } from '../api/ContactsCollection';
-import { useTracker } from 'meteor/react-meteor-data';
+import { useSubscribe, useFind } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { ErrorAlert } from './components/ErrorAlert';
 import { SuccessAlert } from './components/SuccessAlert';
+import { ContactItem } from './components/ContactItem';
 
 export const ContactList = () => {
     const [error, setError] = React.useState(null);
     const [success, setSuccess] = React.useState("");
 
-    const contacts = useTracker(() => {
-        // This will automatically re-run when the ContactsCollection changes
-        return ContactsCollection.find({}, { sort: { createdAt: -1 } }).fetch();
-    });
+    const isLoading = useSubscribe('allContacts');
+    const contacts = useFind(() => ContactsCollection.find({}, { sort: { createdAt: -1 } }));
 
     const removeContact = (contactId) => {
         Meteor.call('contacts.remove', contactId, (error) => {
@@ -45,6 +44,14 @@ export const ContactList = () => {
         }, 3000); // Clear the success message after 3 seconds
     }
 
+    if (isLoading()) {
+        return (
+            <div className="mt-16">
+                <h3 className="text-xs uppercase tracking-wide font-semibold text-gray-500">Loading contacts...</h3>
+            </div>
+        );
+    }
+
     return (
         <>
             <div className="mt-16">
@@ -60,33 +67,7 @@ export const ContactList = () => {
                 
                 <ul role="list" className="divide-y divide-gray-100 py-4">
                     {contacts.map((contact) => (
-                        <li key={contact._id} className="flex border-y border-gray-200 justify-between gap-x-6 py-2">
-                            <div className="flex min-w-0 gap-x-4">
-                                <img alt="" src={contact.imageURL} className="size-12 flex-none rounded-full bg-gray-50" />
-                                <div className="min-w-0 flex-auto">
-                                    <p className="text-sm/6 font-semibold text-gray-900">{contact.name}</p>
-                                    <p className="mt-1 truncate text-xs/5 text-gray-500">{contact.email}</p>
-                                </div>
-                            </div>
-
-                            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                                { contact.createdAt && (
-                                    <p className="text-xs/5 text-gray-500">
-                                        Created on <time dateTime={contact.createdAt}>{contact.createdAt.toDateString()}</time> at <time dateTime={contact.createdAt}>{contact.createdAt.toLocaleTimeString()}</time>
-                                    </p>
-                                )}
-                            </div>
-
-                            <div>
-                                <button
-                                    type='button'
-                                    onClick={() => removeContact(contact._id)}
-                                    className="inline-flex items-center cursor-pointer shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                                >
-                                    Remove
-                                </button>
-                            </div>
-                        </li>
+                        <ContactItem contact={contact} onRemove={removeContact} key={contact._id} />
                     ))}
                 </ul>
             </div>
---
+++ imports/ui/components/ContactItem.jsx
@@ -0,0 +1,39 @@
+import React, { memo } from "react";
+
+export const ContactItem = memo(({ contact, onRemove }) => {
+  const handleRemove = () => {
+    if (onRemove) {
+      onRemove(contact._id);
+    }
+  };
+
+  return (
+    <li key={contact._id} className="flex border-y border-gray-200 justify-between gap-x-6 py-2">
+        <div className="flex min-w-0 gap-x-4">
+            <img alt="" src={contact.imageURL} className="size-12 flex-none rounded-full bg-gray-50" />
+            <div className="min-w-0 flex-auto">
+                <p className="text-sm/6 font-semibold text-gray-900">{contact.name}</p>
+                <p className="mt-1 truncate text-xs/5 text-gray-500">{contact.email}</p>
+            </div>
+        </div>
+
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+            { contact.createdAt && (
+                <p className="text-xs/5 text-gray-500">
+                    Created on <time dateTime={contact.createdAt}>{contact.createdAt.toDateString()}</time> at <time dateTime={contact.createdAt}>{contact.createdAt.toLocaleTimeString()}</time>
+                </p>
+            )}
+        </div>
+
+        <div>
+            <button
+                type='button'
+                onClick={() => handleRemove(contact._id)}
+                className="inline-flex items-center cursor-pointer shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
+            >
+                Remove
+            </button>
+        </div>
+    </li>
+  );
+});
